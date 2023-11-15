28d58e5faea95ac76af5d1f64be8111442852e4e
Hidy97 <csaladi.alexandra@gmail.com>
2023-11-15 13:42:07 +0100

    Könyv osztály elkészítése

--- src/modell/Konyv.java
+++ src/modell/Konyv.java
@@ -5,5 +5,32 @@ package modell;
  * @author csaladi.alexandra
  */
 public class Konyv {
-    
+    private String kolcsNeve;
+    private boolean kolcsonozve;
+
+    public Konyv(String kolcsNeve) {
+        this.kolcsNeve = kolcsNeve;
+        kolcsonozve = false;
+    }
+
+    public String getKolcsNeve() {
+        return kolcsNeve;
+    }
+
+    public void setKolcsNeve(String kolcsNeve) {
+        this.kolcsNeve = kolcsNeve;
+    }
+
+    public boolean isKolcsonozve() {
+        return kolcsonozve;
+    }
+
+    public void setKolcsonozve(boolean kolcsonozve) {
+        this.kolcsonozve = kolcsonozve;
+    }
+
+    @Override
+    public String toString() {
+        return "Konyv{" + "kolcsNeve=" + kolcsNeve + ", kolcsonozve=" + kolcsonozve + '}';
+    }
 }
