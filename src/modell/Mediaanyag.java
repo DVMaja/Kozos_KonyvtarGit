2dbc803f960a52c8ea8484a7981ad73ba8c56bf9
DVMaja <majadreilinger@gmail.com>
2023-11-15 13:50:00 +0100

    biztonsági mentések

--- src/modell/Mediaanyag.java
+++ src/modell/Mediaanyag.java
@@ -2,30 +2,6 @@ package modell;
 
 public class Mediaanyag {
 
-    private int mediaDb;
-    private Konyv[] konyvek;
-
-    public Mediaanyag() {
-        this(5);
-    }
-
-    public Mediaanyag(int db) {
-        mediaDb = 0;
-        konyvek = new Konyv[db];
-    }
-
-    public void kolcsonoz(Konyv konyv) {
-        if (mediaDb < konyvek.length) {
-            konyvek[mediaDb++] = konyv;
-        } else {
-            System.out.println("Nem lehet több könyvet kölcsönözni!");
-        }
-    }
     
-    public void hasznalat(){
-        //itt fog romlani a köny Cd DVD állapota
-        // szakadás, törés, stb lásd a readme-be
-        
-        }
 
 }
