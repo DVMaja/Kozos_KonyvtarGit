fdea1ffa97224d6c521254bcc56b9867f69733bd
Hidy97 <csaladi.alexandra@gmail.com>
2023-11-20 11:34:02 +0100

    Könyvtár kiegészítése: nemVartEs, altalanosHasznalat

--- src/modell/Konyvtar.java
+++ src/modell/Konyvtar.java
@@ -1,9 +1,10 @@
 package modell;
 
 public class Konyvtar {
+
     private Mediaanyag[] mediaanyag;
     private int mediaDb;
-    
+
     public Konyvtar() {
         this(5);
     }
@@ -12,20 +13,43 @@ public class Konyvtar {
         mediaDb = 0;
         mediaanyag = new Mediaanyag[mediaDb];
     }
-    
+
     //Át a könyvtárba
-    public void kolcsonoz(Mediaanyag media){
+    public void kolcsonoz(Mediaanyag media) {
         if (mediaDb < mediaanyag.length) {
             mediaanyag[mediaDb++] = media;
         } else {
             System.out.println("Már kivett 5 könyvet, nem vehet ki többet!");
         }
     }
-    
+
     //használat
-    
-    public Mediaanyag[] getMedia(){
-        Mediaanyag[] mediak = new Mediaanyag[this.mediaanyag.length];
-        return mediak;
+    public void altalanosHasznalat() {
+        for (Mediaanyag media : mediaanyag) {
+            if (media != null) {
+                media.setKolcsonozve(true);
+                media.altalanosHasznalatHatasa();
+            }
+        }
+    }
+
+    public void nemVartEs() {
+        for (Mediaanyag media : mediaanyag) {
+            if (media != null) {
+                media.setKolcsonozve(true);
+                media.NemVartEsemeny();
+            }
+        }
+    }
+
+    public Mediaanyag[] getMedia() {
+        Mediaanyag[] kiadottMediak = new Mediaanyag[this.mediaanyag.length];
+        for (int i = 0; i < mediaDb; i++) {
+            Mediaanyag aktualisMedia = this.mediaanyag[i];
+            if (aktualisMedia != null) {
+                kiadottMediak[i] = aktualisMedia;
+            }
+        }
+        return kiadottMediak;
     }
 }
